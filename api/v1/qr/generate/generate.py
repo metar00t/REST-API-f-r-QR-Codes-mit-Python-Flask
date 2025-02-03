a429cf5296bc0c7acba8a5c88a07d650bb0d3493
David-Paul Adams <david-paul.adams@outlook.de>
2025-02-03 14:18:42 +0100

    initial Commit

--- api/v1/qr/generate/generate.py
+++ api/v1/qr/generate/generate.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template
 import qrcode
 from qrcode.image.styles.moduledrawers.svg import SvgCircleDrawer
 import qrcode.image.svg
@@ -12,16 +12,19 @@ class QrCode:
         self.text = text
     # Methode generate() definiert
     def generate(self):
-
         # TODO Code zum Styling der QR-Codes anpassen
         img = qrcode.make(self.text, image_factory=qrcode.image.svg.SvgPathImage)
         self.text = img.to_string(encoding='unicode')
         # Gibt die SVG (Vektorgrafik) als String zurück
         return f'{self.text}'
     
-    @app.route("/api/v1/qr/generate/<string:text>")
-    def generateQrCode(text):
-        # Erstellung des Objektes
-        qr = QrCode(text)
-        # Aufruf der Methode zur Generierung des Codes mit der Rückgabe von der Methodenausgabe
+@app.route("/")
+def form():
+    return render_template('userinput.html')
+
+@app.route("/", methods=['get', 'post'])
+def passingInput():
+    if request.method == 'POST':
+        input = request.form['text']
+        qr = QrCode(input)
         return qr.generate()
