813262ebde3aa5dec9b40b9dce9fde9b26a7f23a
David-Paul Adams <david-paul.adams@outlook.de>
2025-02-03 14:20:02 +0100

    initial Commit

--- api/v1/qr/generate/generate.py
+++ api/v1/qr/generate/generate.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask
 import qrcode
-from qrcode.image.styles.moduledrawers.svg import SvgCircleDrawer
+from qrcode.image.styles.moduledrawers.svg import SvgPathCircleDrawer
 import qrcode.image.svg
 
 app = Flask(__name__)
@@ -11,20 +11,17 @@ class QrCode:
         # Hier wird der übergebene Wert als Variable deklariert
         self.text = text
     # Methode generate() definiert
-    def generate(self):
-        # TODO Code zum Styling der QR-Codes anpassen
-        img = qrcode.make(self.text, image_factory=qrcode.image.svg.SvgPathImage)
+    def generate_with_style(self):
+        qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+        qr.add_data(self.text)
+        img = qr.make_image(image_factory=qrcode.image.svg.SvgPathImage, module_drawer=SvgPathCircleDrawer())
         self.text = img.to_string(encoding='unicode')
         # Gibt die SVG (Vektorgrafik) als String zurück
         return f'{self.text}'
     
-@app.route("/")
-def form():
-    return render_template('userinput.html')
-
-@app.route("/", methods=['get', 'post'])
-def passingInput():
-    if request.method == 'POST':
-        input = request.form['text']
-        qr = QrCode(input)
-        return qr.generate()
+@app.route("/api/v1/qr/generate/<string:text>")
+def generateQrCode(text):
+    # Erstellung des Objektes
+    qr = QrCode(text)
+    # Aufruf der Methode zur Generierung des Codes mit der Rückgabe von der Methodenausgabe
+    return qr.generate_with_style()
