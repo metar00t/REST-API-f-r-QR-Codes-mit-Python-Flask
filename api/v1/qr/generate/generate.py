5f09f142aa2043a48162761ba7feb2949e6c8d6b
David-Paul Adams <david-paul.adams@outlook.de>
2025-02-03 14:16:27 +0100

    initial Commit

--- api/v1/qr/generate/generate.py
+++ api/v1/qr/generate/generate.py
@@ -1,24 +1,27 @@
 from flask import Flask
 import qrcode
+from qrcode.image.styles.moduledrawers.svg import SvgCircleDrawer
 import qrcode.image.svg
 
 app = Flask(__name__)
 
-@app.route("/api/v1/qr/generate/<string:text>")
-def generateQrCode(text):
+# Klasse QrCode wird erstellt
+class QrCode:
+    def __init__(self, text):
+        # Hier wird der übergebene Wert als Variable deklariert
+        self.text = text
+    # Methode generate() definiert
+    def generate(self):
 
-    method = ""
-
-    if method == 'basic':
-        # Simple factory, just a set of rects.
-        factory = qrcode.image.svg.SvgImage
-    elif method == 'fragment':
-        # Fragment factory (also just a set of rects)
-        factory = qrcode.image.svg.SvgFragmentImage
-    else:
-        # Combined path factory, fixes white space that may occur when zooming
-        factory = qrcode.image.svg.SvgPathImage
-
-    img = qrcode.make(text, image_factory=factory)
-    text = img.to_string(encoding='unicode')
-    return f'{text}'
+        # TODO Code zum Styling der QR-Codes anpassen
+        img = qrcode.make(self.text, image_factory=qrcode.image.svg.SvgPathImage)
+        self.text = img.to_string(encoding='unicode')
+        # Gibt die SVG (Vektorgrafik) als String zurück
+        return f'{self.text}'
+    
+    @app.route("/api/v1/qr/generate/<string:text>")
+    def generateQrCode(text):
+        # Erstellung des Objektes
+        qr = QrCode(text)
+        # Aufruf der Methode zur Generierung des Codes mit der Rückgabe von der Methodenausgabe
+        return qr.generate()
